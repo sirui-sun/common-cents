06325a9c89f9d9c9ad3ab3dc2d522c8eb0d0a95b
Sirui Sun <sirui.sun1@gmail.com>
2014-04-12 01:41:18 -0700

    add landing page

---
+++ app/assets/javascripts/landing.js
@@ -0,0 +1,8 @@
+//= require jquery
+//= require jquery_ujs
+//= require config
+//= require cufon
+//= require font
+//= require script
+//= require jquery.mousewheel
+//= require jquery.jscrollpane.min
